82d83412415d938a4337015c58797a101c3fbcfe
Ryunosuke TAKEDA <takkerue@gmail.com>
2015-04-22 18:44:22 +0900

    add Spec mindmap fime

---
+++ docs/01Spec/require.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1429695714949" ID="ID_1371483469" MODIFIED="1429695735281" TEXT="&#x30b9;&#x30c8;&#x30c3;&#x30d7;&#x30a6;&#x30a9;&#x30c3;&#x30c1;&#x4ed5;&#x69d8;">
+<node CREATED="1429695798176" ID="ID_1071473025" MODIFIED="1429695798176" POSITION="right" TEXT=""/>
+</node>
+</map>
